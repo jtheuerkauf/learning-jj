002a46e86165b80cd515958590db44ec034aed44
Joe Theuerkauf <joe@theuerkauf.dev>
2026-01-25 12:45:22 -0500

    add comment to main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-/// A hello JJ program
+/// A "Hello JJ!" program
 ///
 /// I don't think I'll find a new job fast enough.
 
+// This is the main function
 fn main() {
     print("Hello, JJ!");
 }
 
-// This prints.
+// This prints a message.
 fn print(m: &str) {
     println!("{m}");
 }
