d6feabdc112f29594aa7b598104e2f4111bb3b6f
Joe Theuerkauf <joe@theuerkauf.dev>
2026-01-24 19:53:53 -0500

    create hello/goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// I don't think I'll find a new job fast enough.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, JJ!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, git!");
 }
