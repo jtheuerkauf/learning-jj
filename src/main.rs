f3c22a10aad335263f3eb6b048f3cefa0cdfccf2
Joe Theuerkauf <joe@theuerkauf.dev>
2026-01-26 21:22:35 -0500

    Refactor printing

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@
 /// This is important, so don't remove it!
 
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, JJ!");
+    print("Goodbye, git!");
 }
 
-fn print_hello() {
-    println!("Hello, JJ!");
-}
-
-fn print_goodbye() {
-    println!("Goodbye, git!");
+// A function that prints
+fn print(m: &str) {
+    println!("{m}");
 }
