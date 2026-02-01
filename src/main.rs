0f76819def94e8d9a27042864813a20adeb319c2
Joe Theuerkauf <joe@theuerkauf.dev>
2026-02-01 09:28:26 -0500

    Create hello/goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// Comments that don't explain something are useless. Like this one.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, JJ!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, Git!");
 }
