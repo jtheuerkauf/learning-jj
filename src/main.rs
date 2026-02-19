7b3e8685f5677d30e696c695f304fa1eb914013b
Joe Theuerkauf <joe@theuerkauf.dev>
2026-02-19 00:05:30 -0500

    Create hello/goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// This is a tutorial program. The code is not the point.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, JJ!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, Git!");
 }
